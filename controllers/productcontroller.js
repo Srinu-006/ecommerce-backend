5b7a3221aff9113bae3b741646234da0c3cefa3c
Srinivas Donka <srinivasdonka66@gmail.com>
2026-03-05 11:06:14 +0530

    added sorting and pagination

--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -2,11 +2,27 @@ const Product = require('./../models/productmodel');
 
 exports.getallproducts = async (req, res) => {
     try{
+        //Filtering
         const filter = {};
         if(req.query.category){
             filter.category = req.query.category;
         }
-        const products = await Product.find(filter);
+        let query = Product.find(filter);
+
+        //Sorting
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        }
+
+        //pagination
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 5;
+        skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+
+        
+        const products = await query;
         res.status(200).json({
             status: 'Success',
             results: products.length,
